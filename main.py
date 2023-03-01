0e2043f7b8ebe0c345f378c8cef60601914d246f
kaczynskistufts <117485266+kaczynskistufts@users.noreply.github.com>
2023-03-01 16:01:17 -0500

    added more things

--- main.py
+++ main.py
@@ -1,21 +1,48 @@
 temp = 0
-humid = 0
 strip: neopixel.Strip = None
+humid = 0
+lux = 0
+def checkTemp():
+    if temp > 75:
+        strip.show_color(neopixel.colors(NeoPixelColors.RED))
+        pins.digital_write_pin(DigitalPin.P8, 1)
+    else:
+        strip.show_color(neopixel.colors(NeoPixelColors.GREEN))
+        pins.digital_write_pin(DigitalPin.P8, 0)
 def showData():
     OLED.write_string_new_line("temperature")
     OLED.write_num_new_line(temp)
     OLED.write_string_new_line("humidity")
     OLED.write_num_new_line(humid)
+    OLED.write_string_new_line("lux")
+    OLED.write_num_new_line(lux)
+def changeBrightness():
+    if lux > 200:
+        strip.clear()
+        strip.show()
+    else:
+        strip.show()
+def controlLightBulb():
+    if lux > 400:
+        pins.digital_write_pin(DigitalPin.P2, 0)
+    else:
+        pins.digital_write_pin(DigitalPin.P2, 1)
+
 def getData():
-    global temp, humid
+    global temp, humid, lux
     temp = grove.aht20_read_temperature_f()
     humid = grove.aht20_read_humidity()
+    lux = BH1750.get_intensity()
 
 def on_forever():
     global strip
     strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
+    BH1750.on()
     OLED.init(128, 64)
     getData()
     showData()
+    checkTemp()
     basic.pause(5000)
+    changeBrightness()
+    controlLightBulb()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,20 +1,52 @@
 let temp = 0
-let humid = 0
 let strip: neopixel.Strip = null
+let humid = 0
+let lux = 0
+function checkTemp () {
+    if (temp > 75) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+        pins.digitalWritePin(DigitalPin.P8, 1)
+    } else {
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+        pins.digitalWritePin(DigitalPin.P8, 0)
+    }
+}
 function showData () {
     OLED.writeStringNewLine("temperature")
     OLED.writeNumNewLine(temp)
     OLED.writeStringNewLine("humidity")
     OLED.writeNumNewLine(humid)
+    OLED.writeStringNewLine("lux")
+    OLED.writeNumNewLine(lux)
+}
+function changeBrightness () {
+    if (lux > 200) {
+        strip.clear()
+        strip.show()
+    } else {
+        strip.show()
+    }
+}
+function controlLightBulb () {
+    if (lux < 400) {
+        pins.digitalWritePin(DigitalPin.P2, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    }
 }
 function getData () {
     temp = grove.aht20ReadTemperatureF()
     humid = grove.aht20ReadHumidity()
+    lux = BH1750.getIntensity()
 }
 basic.forever(function () {
     strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
+    BH1750.on()
     OLED.init(128, 64)
     getData()
     showData()
+    checkTemp()
     basic.pause(5000)
+    changeBrightness()
+    controlLightBulb()
 })
